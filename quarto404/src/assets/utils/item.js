8593a85ab57ca536c3ab9a756939072959bfd85e
filiperuizb <filiperuizboligon9@gmail.com>
2025-06-07 01:20:57 -0300

    utils/item

---
+++ quarto404/src/assets/utils/item.js
@@ -0,0 +1,11 @@
+/* Pessoal aqui será o arquivo do que será
+    clicável no jogo, ou seja, itens que o jogaodr poderá
+    clicar e interagir precisam ser recortados do fundo 
+    e colados o png do objeto aqui com o nome de marcação
+*/
+const itens = [
+    {
+        nome: '',
+        caminhoImagem: '',
+    }
+]
