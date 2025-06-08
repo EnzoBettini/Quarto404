95a628d22c8e95ea11695c37fd0dc1948e8e4d90
Gabriel Arantes <bielarantes00oo@gmail.com>
2025-06-07 23:23:57 -0300

    criação do overlay para aplicações, adequacao da lógica de rotas para cada tipo de pasta e adequações das pastas aplicar

--- quarto404/src/assets/utils/item.js
+++ quarto404/src/assets/utils/item.js
@@ -4,9 +4,12 @@
     e colados o png do objeto aqui com o nome de marcação
 */
 // O CAMINHO DA PASTA DE IMAGENS PROS ITENS: assets/images/itens
-const itens = [
+export const itens = [
     {
-        nome: '',
-        caminhoImagem: '',
+        nome:'televisao_401',
+        caminhoImagem:'/itens/televisao_401.png',
+        //a pasta não deve possuir o caminho ../, visto que ele nao suporta esse tipo de procura, por isso
+        //a pasta itens foi transferida para o public, por favor inserir as imagens de overlay para a pasta 
+        //itens
     }
 ]
---
+++ quarto404/src/components/interacoes/sala401.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="item-container">
+    <button onclick="alert(1)">
+      <Overlay nome="televisao_401"/>
+    </button>
+  </div>
+  
+  
+</template>
+
+<script setup>
+import Overlay from '../efeitos/Overlay.vue';
+
+
+</script>
+
+<style>
+
+.item-container {
+ 
+  position: absolute;
+  top: -420%; 
+  left: 12%; 
+  transform: translate(-50%, -50%); 
+  width: 13vw; 
+  cursor: pointer;
+  pointer-events: auto; 
+  border: 3px solid transparent; 
+  border-radius: 8px;
+  transition: border-color 0.2s ease-in-out; 
+}
+
+.item-container:hover {
+  border-color: #a7a7a777; 
+}
+button{
+   background: none;
+
+  /* Remove a borda */
+  border: none;
+  padding: 0;
+}
+</style>
